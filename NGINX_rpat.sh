8d5afec9e86c488276dea18c025dff467c12b1e1
magicweirdoo <102423839+magicweirdoo@users.noreply.github.com>
2022-09-18 19:09:39 +0200

    NGINX reverse proxy automation too (rpat)

---
+++ NGINX_rpat.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SitesAvailableLocation="/etc/nginx/sites-available/"		#Location of the "sites-available" directory
+SitesEnabledLocation="/etc/nginx/sites-enabled/"		#Location of the "sites-enabled" directory
+
+
+clear 								#clears the Terminal for clean astetic
+
+echo "Welcome to the Nginx Reverse Proxy Automation tool!"
+echo " "
+echo "╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━╮"
+echo "┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃"
+echo "┃╰━╯┣━━┳╮╭┳━━┳━┳━━┳━━╮┃╰━╯┣━┳━━┳╮╭┳╮╱╭╮"
+echo "┃╭╮╭┫┃━┫╰╯┃┃━┫╭┫━━┫┃━┫┃╭━━┫╭┫╭╮┣╋╋┫┃╱┃┃"
+echo "┃┃┃╰┫┃━╋╮╭┫┃━┫┃┣━━┃┃━┫┃┃╱╱┃┃┃╰╯┣╋╋┫╰━╯┃"
+echo "╰╯╰━┻━━╯╰╯╰━━┻╯╰━━┻━━╯╰╯╱╱╰╯╰━━┻╯╰┻━╮╭╯"			#Animation
+echo "╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃"
+echo "╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯"
+echo "╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╭━━━━╮╱╱╱╱╭╮"
+echo "┃╭━╮┃╱╭╯╰╮╱╱╱╱╱╱╱╭╯╰╮╱╱╱╱╱╱╱┃╭╮╭╮┃╱╱╱╱┃┃"
+echo "┃┃╱┃┣╮┣╮╭╋━━┳╮╭┳━┻╮╭╋┳━━┳━╮╱╰╯┃┃┣┻━┳━━┫┃"
+echo "┃╰━╯┃┃┃┃┃┃╭╮┃╰╯┃╭╮┃┃┣┫╭╮┃╭╮╮╱╱┃┃┃╭╮┃╭╮┃┃"
+echo "┃╭━╮┃╰╯┃╰┫╰╯┃┃┃┃╭╮┃╰┫┃╰╯┃┃┃┃╱╱┃┃┃╰╯┃╰╯┃╰╮"
+echo "╰╯╱╰┻━━┻━┻━━┻┻┻┻╯╰┻━┻┻━━┻╯╰╯╱╱╰╯╰━━┻━━┻━╯"
+echo " "
+
+echo "Enter your listening port: " 
+read listening_port
+echo " "
+
+echo "Enter the domain (server_name: )"
+read server_name
+echo " "
+
+echo "Enter the proxy_pass: "
+read proxy_pass
+echo " " 
+
+
+printf "\nserver{\n	listen $listening_port;\n	server_name $server_name;\n\n	location / {\n		proxy_pass $proxy_pass;\n	}\n}" > $SitesAvailableLocation$server_name
+#
+#The printf statement writes this file(Example file):
+#
+#server {
+#	listen 80;					#In this case the port '80' is the listening port
+#	server_name sub.domain.com			#In this case the server name (Domain name) is 'sub.domain.com'
+#
+#	location / {
+#		proxy_pass http://localhost:8080;	#In this case the proxy pass is 'http://localhost:8080'
+#	}
+#}
+
+ln -s $SitesAvailableLocation$server_name $SitesEnabledLocation 
